0af1d1d0df627e78c7f058f91e25dd4aa58d7de4
Anoop Makam <anoopmakam@Anoops-MacBook-Air.local>
2020-10-05 18:36:16 -0700

    Added AddingMachine.java

---
+++ cseSem5/AddingMachine.java
@@ -0,0 +1,49 @@
+/* 
+ * @author : Anoop Makam
+ */
+
+
+package cse360assignment02;
+/* 
+ * Importing package called cse360assignment02
+ */
+
+public class AddingMachine {
+  private int total;
+  /*
+   * public method , initializes the private variable 
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /*
+   * getTotal() gets total 
+   * @return 0
+   */
+  public int getTotal () {
+    return 0;
+  }
+  /*
+   * void function to add 
+   * @param value to be added
+   */
+  public void add (int value) {
+  }
+  /*
+   * void function to subtract 
+   * @param value to be subtracted
+   */
+  public void subtract (int value) {
+  }
+  /*
+   * @return concatination of String
+   */
+  public String toString () {
+    return "";
+  }
+  /*
+   * void funciton , used to clear.
+   */
+  public void clear() {
+  }
+}
